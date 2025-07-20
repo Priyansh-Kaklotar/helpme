892ca423c55ace531db34367085b9f2c4d873cd0
sharadvyas123 <sharadvyas132@gmail.com>
2025-07-20 15:02:38 +0530

    middle ware try karyu hatu no thayu check karje

--- middleware.js
+++ middleware.js
@@ -1,20 +1,36 @@
-// middleware.js
+// import { NextResponse } from 'next/server';
+
+// const protectedRoutes = ['/', '/about'];
+
+// export function middleware(request) {
+//   // console.log('Middleware hit:', pathname);
+
+//   // const { pathname } = request.nextUrl;
+//   // const token = request.cookies.get('access_token')?.value;
+
+//   // const isProtected = protectedRoutes.includes(pathname);
+
+//   // if (isProtected && !token) {
+//   //   return NextResponse.redirect(new URL('/login', request.url));
+//   // }
+
+//   // return NextResponse.next();
+//   return NextResponse.redirect(new URL('/login', request.url));
+// }
+
+// export const config = {
+//     matcher: ['/((?!api|_next|.*\\..*|login).*)'],
+
+// };
+
+
 import { NextResponse } from 'next/server';
 
-const protectedRoutes = ['/' , '/about'];
-
 export function middleware(request) {
-  const { pathname } = request.nextUrl;
-  const token = request.cookies.get('access_token');
-
-  const isProtected = protectedRoutes.includes(pathname);
-
-  if (isProtected && !token) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
+  console.log('✅ Middleware triggered at', request.nextUrl.pathname);
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: ['/', '/about'],
+  matcher: ['/', '/about', '/((?!_next|api|.*\\..*).*)'],
 };
