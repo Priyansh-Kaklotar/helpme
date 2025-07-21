e8342be8f6b7eceb532cd1256236bd4fe2a425bc
sharadvyas123 <sharadvyas132@gmail.com>
2025-07-21 19:00:03 +0530

    delete kari nakhyu

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import { unique } from "next/dist/build/utils";
 
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
   name: {
     type: String,
     required: true,
@@ -16,21 +15,6 @@ const UserSchema = new mongoose.Schema({
 export default mongoose.models.User || mongoose.model("User", UserSchema);
 
 // aa simple user model chhe ane aama have use karvo hoy to aam karvanu
-=======
-    name : {
-        type: String,
-        required: true,
-    },
-    email:{
-        type : String,
-        unique :true,
-    }
-})
-
-export default mongoose.models.User || mongoose.model('User', UserSchema);
-
-// aa simple user model chhe ane aama have use karvo hoy to aam karvanu 
->>>>>>> 892ca423c55ace531db34367085b9f2c4d873cd0
 // src/app/api/user/route.js or route.ts
 // import { connectToDatabase } from '@/lib/mongodb';
 // import User from '@/models/User';
