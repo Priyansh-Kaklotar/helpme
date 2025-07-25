a439b498161cf7f3207d6065562812a1ef2e9cb8
sharadvyas123 <sharadvyas132@gmail.com>
2025-07-25 21:03:16 +0530

    otp success

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -20,8 +20,8 @@ export async function POST(req) {
     const transporter  = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user : process.env.SMPT_USER,
-        pass: process.env.SMPT_PASS 
+        user : process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
       },
     });
 
