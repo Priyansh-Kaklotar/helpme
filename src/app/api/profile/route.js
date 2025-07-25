10816fa13f18a70fffc4543fd125022e4515c9fa
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-07-25 23:12:25 +0530

    register route change

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,32 +1,37 @@
-import User from '@/src/models/User.model'
+import User from "@/src/models/User.model";
 import connectToDatabase from "@/src/lib/mongodb";
 
-export async function GET(reqest) {
-    const url = new URL(reqest.url);
-    const userid = url.searchParams.get('id');
-    console.log(userid);
-    await connectToDatabase();
-    const user =await User.findById(userid);
-    const type = user.type;
-    const userpincode = user.pincode;
-    if(type === "Customer"){
-        const allProvider =await User.find({type : "Service Provider",pincode: userpincode})
-        return new Response(JSON.stringify(allProvider),{
-            status:200,
-            headers:{
-                "Content-Type": "application/json",
-            }
-        })
-    }
-    else{
-        // provider hase
-        const nearbyUser =await User.find({pincode:userpincode , type : "Customer"}); //same pincode hoy etle najik na customer male pachi address ne strip kari ne te walo logic lagavsu pan atyare aa 
+export async function GET(request) {
+  const url = new URL(request.url);
+  const userid = url.searchParams.get("id");
+  console.log(userid);
+  await connectToDatabase();
+  const user = await User.findById(userid);
+  const type = user.type;
+  const userpincode = user.pincode;
+  if (type === "Customer") {
+    const allProvider = await User.find({
+      type: "Service Provider",
+      pincode: userpincode,
+    });
+    return new Response(JSON.stringify(allProvider), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } else {
+    // provider hase
+    const nearbyUser = await User.find({
+      pincode: userpincode,
+      type: "Customer",
+    }); //same pincode hoy etle najik na customer male pachi address ne strip kari ne te walo logic lagavsu pan atyare aa
 
-        return new Response(JSON.stringify(nearbyUser),{
-            status:200,
-            headers:{
-                "Content-Type": "application/json",
-            }
-        })
-    }
+    return new Response(JSON.stringify(nearbyUser), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
