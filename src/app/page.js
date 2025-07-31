7868865d83c8af837da63210a466782663fd1df4
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-07-31 21:43:51 +0530

    toggle theme added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import ThemeToggleButton from "../components/ui/theme-toggle-button";
 export default function Home() {
   return (
     <>
-    
+      <ThemeToggleButton />
+      <h1>hello world</h1>
     </>
   );
 }
