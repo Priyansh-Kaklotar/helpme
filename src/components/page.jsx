4ec55c57ff9390b8c7df93e9f72a415ea55ff38c
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-08-03 19:01:20 +0530

    p

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import { ThemeProvider } from "./ui/theme-provider";
 import ThemeToggleButton from "@/src/components/ui/theme-toggle-button";
 
 function Navbar() {
   return (
     <>
-    {/* <div className="fixed w-full z-50"></div>
+      {/* <div className="fixed w-full z-50"></div>
       <div className="bg-gradient-to-r from-purple-600 to-blue-600 p-4">
         <div className="max-w-7xl mx-auto">
           <nav>
@@ -26,19 +26,23 @@ function Navbar() {
       </div> */}
       <ThemeProvider attribute="class"></ThemeProvider>
       <div className="dark:bg-gray-900 bg-white ">
-          <nav className="dark:bg-gray-900 bg-white border-b dark:border-gray-700">
-            <ul className="flex justify-between items-center bg-gray-800 text-white p-4">
-              <div>
-                <ThemeToggleButton />
-              </div>
-              <div className="flex gap-12">
-                <li><Link href={"/home"}>Home</Link></li>
-                <li>contact</li>
-                <li><Link href="/about">About</Link></li>
-              </div>
-            </ul>
-          </nav>
-        </div>
+        <nav className="dark:bg-gray-900 bg-white border-b dark:border-gray-700">
+          <ul className="flex justify-between items-center bg-gray-800 text-white p-4">
+            <div>
+              <ThemeToggleButton />
+            </div>
+            <div className="flex gap-12">
+              <li>
+                <Link href={"/home"}>Home</Link>
+              </li>
+              <li>Contact</li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
