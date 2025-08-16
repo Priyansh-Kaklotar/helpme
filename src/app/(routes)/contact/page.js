3c0bc9781925f9f3509006ab8bcb53f2d52e56d4
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-08-16 16:29:33 +0530

    p

--- src/app/(routes)/contact/page.js
+++ src/app/(routes)/contact/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>this is the contact page</div>;
+  return <div>this is is the contact page hello world</div>;
 }
 
 export default page;
